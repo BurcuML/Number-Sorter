6375f298ba4294a1af0dd89ce67657dfd7f836f4
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-02-23 05:52:38 +0300

    updated

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 }
@@ -35,13 +35,20 @@ const bubbleSort = (array) => {
 }
 
 //Selection Sort
-/*const selectionSort = (array) => {
+const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     let minIndex = i;
 
     for (let j = i + 1; j < array.length; j++) {
-
+      if (array[j] < array[minIndex]) {
+        minIndex = j;   
+      }
     }
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
   }
-}*/
+  return array
+}
+
 sortButton.addEventListener("click", sortInputArray);
