adb6674000bb5755c7c9951988d4eb59118a7f64
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-02-23 06:29:48 +0300

    updated

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ const sortInputArray = (event) => {
   updateUI(sortedValues);
 }
 
+/*
+(a, b) => a - b karşılaştırma fonksiyonu, iki değeri (a ve b) çıkartarak karşılaştırma yapar. 
+Eğer a, b'den küçükse sonuç negatif olur ve a önce gelir; eğer a, b'den büyükse sonuç pozitif olur ve b önce gelir; eğer eşitse sonuç sıfır olur ve sıralama değişmez.
+Bu şekilde sıralama, sayıların artan düzende yerleştirilmesini sağlar. 
+Bu nedenle, bu yöntem sayısal verilerin doğru bir şekilde sıralanması için kullanılır.
+*/ 
+const sortedValues = inputValues.sort((a, b) => {
+  return a-b
+});
+
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
@@ -49,6 +59,22 @@ const selectionSort = (array) => {
     array[minIndex] = temp;
   }
   return array
+}
+//Insertion Sort = Insertion sort algoritması, dizideki elemanları sıralamak için kullanılan basit bir sıralama algoritmasıdır. 
+//Çalışma prensibi, diziyi parça parça sıralamak ve her yeni elemanı doğru pozisyona yerleştirmektir.
+//Insertion sort'un zaman karmaşıklığı en kötü durumda O(n^2) iken, en iyi durumda O(n) ve ortalama durumda O(n^2) olarak kabul edilir.
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+return array
 }
 
 sortButton.addEventListener("click", sortInputArray);
